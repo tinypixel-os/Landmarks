5bc78351d2e69605d4ca25f37e5c765bb0394422
tinypixel <him23code@gmail.com>
2024-07-01 12:55:59 +0530

    Create a badge for landmark

--- Landmarks/View/Badges/Badge.swift
+++ Landmarks/View/Badges/Badge.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct Badge: View {
+    var badgeSymbols: some View {
+        ForEach(0..<8) {
+            index in
+            RotatedBadgeSymbol(
+                    angle: .degrees(Double(index) / Double(8)) * 360.0
+                )
+            }.opacity(0.5)
+        }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            BadgeBackground()
+
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
     }
 }
 
